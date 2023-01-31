d0a149350a76adf741d31f3b1f154607bc41e06c
christofs <c.schoech@gmail.com>
2023-01-31 20:33:20 +0100

    First batch of text files + unmodified scripts

---
+++ scripts/annotate.py
@@ -0,0 +1,99 @@
+"""
+Script to count verbs of "inner life" in collections of unannotated files. 
+Uses spacy for annotation. (Optimization: use TreeTagger with PRESTO model for 18th century.)
+Outputs verb count information in the same style as Lou's and/or Diana's scripts, for compatibility. 
+
+Script written by Christof Schöch (Trier), January 2023. 
+"""
+
+
+# === Imports ===
+
+#== Basics
+import os
+import random
+import re
+from os.path import join
+import numpy as np
+import glob
+
+#== Data
+import pandas as pd
+import seaborn as sns
+
+# Linguistic annotation
+import spacy
+import fr_dep_news_trf
+
+
+# === Global variables ===
+
+# Files outside the innerlife repository
+#textfolder = join("/", "media", "christof", "Data", "Github", "mimotext", "roman18", "plain", "files", "*.txt") #roman18
+textfolder = join("/", "media", "christof", "Data", "Github", "eltec", "ELTeC-fra", "plain1", "*.txt") #ELTeC-fra
+
+# Local data
+workdir = join(os.path.realpath(os.path.dirname(__file__)), "..")
+annotatedfolder = join(workdir, "data", "fra19", "annotated", "")
+
+
+# === Functions === 
+
+
+def read_textfile(file): 
+    """
+    Reads a plain text file from the text repository. 
+    Returns: string (containing the complete plain text). 
+    """
+    with open(file, "r", encoding="utf8") as infile: 
+        text = infile.read()
+    #if len(text) > 50000: 
+    #    text = text[0:(int(len(text)/10))]
+    #elif len(text) > 20000: 
+    #    text = text[0:(int(len(text)/5))]
+    text = text[0:1000]
+    return text
+
+
+def annotate_text(text, nlp): 
+    """
+    Annotates the text using the spacy NLP model activated initially.
+    Returns: list (list of tokens with annotation according to spacy data model)
+    """
+    nlp.max_length = len(text) + 1000
+    annotated = nlp(text)
+    print([(w.text, w.pos_, w.lemma_, w.tag_, w.dep_, w.morph, w.ent_type) for w in annotated[0:25]])
+    return annotated
+
+
+def save_annotated(basename, annotated): 
+    serialized = [t.text + "\t" + t.pos_ + "\t" + t.lemma_ for t in annotated]
+    serialized = "\n".join(serialized)
+    annotatedfile = join(annotatedfolder, basename + ".csv")
+    with open(annotatedfile, "w", encoding="utf8") as outfile: 
+        outfile.write(serialized)
+
+
+# === Coordination function === 
+
+def main():
+    """
+    Coordinates the process. 
+    Loads nlp model from spacy. 
+    Then loops over each text to annotate, and saves annotation to disk. 
+    """
+    nlp = spacy.load("fr_dep_news_trf")
+    spacy.prefer_gpu()
+    progress = 0
+    for file in glob.glob(textfolder)[0:5]: 
+        basename, ext = os.path.basename(file).split(".")
+        text = read_textfile(file)
+        annotated = annotate_text(text, nlp)
+        save_annotated(basename, annotated)
+        print(progress, basename, str(len(text)) + ": done.")
+        progress +=1
+
+main()
+
+
+
